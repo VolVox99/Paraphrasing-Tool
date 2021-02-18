2ef59d23a06ed1d1f3e9f88c23e95aa9e6e1c9a8
VolVox99 <alirastegar123@gmail.com>
2021-02-17 17:27:24 -0800

    updated ignore conditions

--- ignore.py
+++ ignore.py
@@ -1,6 +1,17 @@
+
 def should_ignore(word: str) -> bool:
     with open('ignorelist.txt') as file:
-        words = file.readlines()
-    
-    return word.lower() in words
-    
+        words_to_ignore = file.readlines()
+
+
+    conditions = [
+        #if all chars are not letters from alphabet
+        not word.isalpha(),
+        word.lower() in words_to_ignore,
+        word.isnumeric(),
+        #if first letter is capital = proper noun
+        word[0].isupper()
+    ]
+
+
+    return any(conditions)
